db957331e91c0e7c1c0532cb7137978af25e6d2f
Hespi <sardi.andras@gmail.com>
2020-10-26 13:10:37 +0100

    Add files via upload

---
+++ szerdahomerseklet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int d2tomb[7][3] = {
+        {1,2,3},
+        {10,20,30},
+        {14,32,12},
+        {53,43,32},
+        {0,0,0},
+        {2,42,231},
+        {8,7,8}
+
+    };
+    printf("Szerdan Reggel a homerseklet: %d\n",d2tomb[2][0]);
+    return 0;
+}
